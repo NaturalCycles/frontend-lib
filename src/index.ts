fedbb78cd3e2762cecccda629f91e899e66df1a4
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-05-13 02:34:03 +0200

    fix: bundlephobia

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
+import ky from 'ky'
 import { getKy } from './http/getKy'
 import { GetKyOptions } from './http/getKy.model'
 import { loadScript } from './script/script.util'
 import { topbar, TopBarOptions } from './vendor/topbar/topbar'
 
-export { TopBarOptions, topbar, getKy, GetKyOptions, loadScript }
-
-export * as ky from 'ky'
+export { TopBarOptions, topbar, getKy, GetKyOptions, loadScript, ky }

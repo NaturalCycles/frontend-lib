033d068956c1c30d1f3d883ed7acee269250012c
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-05-13 02:51:04 +0200

    fix: export more ky types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
+import type {
+  AfterResponseHook,
+  BeforeRequestHook,
+  BeforeRetryHook,
+  NormalizedOptions,
+  RetryOptions,
+} from 'ky'
 import ky from 'ky'
 import { getKy } from './http/getKy'
 import { GetKyOptions } from './http/getKy.model'
 import { loadScript } from './script/script.util'
 import { topbar, TopBarOptions } from './vendor/topbar/topbar'
 
-export { TopBarOptions, topbar, getKy, GetKyOptions, loadScript, ky }
+export {
+  TopBarOptions,
+  topbar,
+  getKy,
+  GetKyOptions,
+  loadScript,
+  ky,
+  BeforeRetryHook,
+  AfterResponseHook,
+  BeforeRequestHook,
+  RetryOptions,
+  NormalizedOptions,
+}
